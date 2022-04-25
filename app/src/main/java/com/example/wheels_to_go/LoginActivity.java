f77a458a509ff6e0103f116e0aa5e16c3b4556f8
Gabriel <gabriel51coyote@gmail.com>
2022-04-24 23:44:54 -0500

    Basic gui done

--- app/src/main/java/com/example/wheels_to_go/LoginActivity.java
+++ app/src/main/java/com/example/wheels_to_go/LoginActivity.java
@@ -1,14 +1,55 @@
 package com.example.wheels_to_go;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    private TextView register_btn, login_btn;
+
+    /* ********************************************************************** */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register_btn = findViewById(R.id.textView_register);
+        register_btn.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+                });
+
+
+        login_btn = findViewById(R.id.textView_login);
+        login_btn.setOnClickListener(view -> { logUserIn();});
     }
 
+
+
+    private void logUserIn(){
+
+        if(true){
+
+
+            // Switch to main activity
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            alertDialog("Invalid Credentials😐");
+        }
+    }
+    /* ********************************************************************** */
+    /* FUNCTION NAME:    alertDialog
+       INPUT:            A String
+       OUTPUT:           n/a
+       PURPOSE:          To make the code more readable,
+                         outputs an alert style text box    */
+    private void alertDialog(String text){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/wheels_to_go/RegisterActivity.java
+++ app/src/main/java/com/example/wheels_to_go/RegisterActivity.java
@@ -1,14 +1,31 @@
 package com.example.wheels_to_go;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+
+    /* ********************************************************************** */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+
+
+
+    /* ********************************************************************** */
+    /* FUNCTION NAME:    alertDialog
+       INPUT:            A String
+       OUTPUT:           n/a
+       PURPOSE:          To make the code more readable,
+                         outputs an alert style text box    */
+    private void alertDialog(String text){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
